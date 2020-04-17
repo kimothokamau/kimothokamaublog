8130a473e2bfc02ea4be74dda3deb23bb7bec21b
kimothokamau <kimothokamau@protonmail.com>
2020-04-17 13:56:33 +0300

    add disqus and wordpress2016 theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -13,7 +13,7 @@ Wordpress2016.overrideThemeStyles = () => {
 
 delete Wordpress2016.googleFonts
 
-const typography = new Typography(githubTheme)
+const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
 if (process.env.NODE_ENV !== `production`) {
